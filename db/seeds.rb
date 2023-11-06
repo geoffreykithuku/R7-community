8194f6c338b435b444d1ac3036359d8852110a84
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-06 15:40:56 +0300

    has_and_belongs_to_many

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(id: 1, name: 'Jay', nickname: 'dadjokes')
+User.create(id: 2, name: 'Pasan', nickname: 'pasanpr')
+User.create(id: 5, name: 'Alena', nickname: 'sketchings')
+Forum.create(id: 1, name: 'Ruby', public: true)
+Forum.create(id: 3, name: 'PHP', public: true)
+Forum.create(id: 4, name: 'iOS', public: true)
+Forum.create(id: 6, name: 'SQL', public: true)
