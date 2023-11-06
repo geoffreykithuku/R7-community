7da108613fe461e815d21678e2d1e0711c747247
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-06 16:21:12 +0300

    has_one

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_122524) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_131435) do
   create_table "forums", force: :cascade do |t|
     t.string "name"
     t.boolean "public"
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_122524) do
     t.integer "forum_id", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "twitter"
+    t.string "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
@@ -30,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_122524) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
